9fe0b0bc4bf1aeaac8984914187fe284d14d5670
zhizhen <lishuai@papegames.net>
2023-06-30 11:31:58 +0800

    2490. 回环句

--- str_demo/src/char_demo.rs
+++ str_demo/src/char_demo.rs
@@ -53,3 +53,32 @@ pub fn maximum_value_lc(strs: Vec<String>) -> i32 {
     strs.into_iter().map(|s| parse(s.as_bytes())).max().unwrap()
 }
 
+/// *** 2490. 回环句
+/// 句子 是由单个空格分隔的一组单词，且不含前导或尾随空格。
+///  例如，"Hello World"、"HELLO"、"hello world hello world" 都是符合要求的句子。
+///  单词 仅 由大写和小写英文字母组成。且大写和小写字母会视作不同字符。
+///
+/// 如果句子满足下述全部条件，则认为它是一个 回环句 ：
+///
+/// 单词的最后一个字符和下一个单词的第一个字符相等。
+/// 最后一个单词的最后一个字符和第一个单词的第一个字符相等。
+/// 例如，"leetcode exercises sound delightful"、"eetcode"、"leetcode eats soul" 都是回环句。
+/// 然而，"Leetcode is cool"、"happy Leetcode"、"Leetcode" 和 "I like Leetcode" 都 不 是回环句。
+/// 给你一个字符串 sentence ，请你判断它是不是一个回环句。如果是，返回 true ；否则，返回 false 。
+
+// lc比较优秀做法
+pub fn is_circular_sentence_0(sentence: String) -> bool {
+    sentence.chars().enumerate().all(|(i,c)| {
+        if c == ' ' && sentence.chars().nth(i+1) != sentence.chars().nth(i-1) {
+            false
+        } else {
+            true
+        }
+    }) && sentence.chars().next() == sentence.chars().last()
+}
+
+// lc 非常秀的做法
+pub fn is_circular_sentence_1(sentence: String) -> bool {
+   let (N, s) = (sentence.len(), sentence.as_bytes());
+    s[0] == s[N-1] && (0..N).filter(|&i| s[i] == b' ').all(|i| s[i-1] == s[i+1])
+}
--- str_demo/src/lib.rs
+++ str_demo/src/lib.rs
@@ -7,10 +7,14 @@ pub fn add(left: usize, right: usize) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    use char_demo::*;
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+    #[test]
+    fn test1() {
+        is_circular_sentence_0("leetcode exercises sound delightful".to_string());
     }
 }
