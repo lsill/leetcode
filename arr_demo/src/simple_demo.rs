b63ee09e813860a07d69668fa13f96b6eb4e51ba
lsill <924867750@qq.com>
2023-07-01 18:01:57 +0800

    1. 两数之和

--- arr_demo/src/simple_demo.rs
+++ arr_demo/src/simple_demo.rs
@@ -89,3 +89,25 @@ pub fn reconstruct_matrix_try(upper: i32, lower: i32, colsum: Vec<i32>) -> Vec<V
     }
     res
 }
+
+/// 1. 两数之和
+/// 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+/// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+/// 你可以按任意顺序返回答案。
+///
+/// 示例 1：
+/// 输入：nums = [2,7,11,15], target = 9
+/// 输出：[0,1]
+/// 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut kv = std::collections::HashMap::new();
+    nums.into_iter().
+        enumerate().
+        find_map(|(k,v)| match kv.get(&(target - v)){
+            Some(j) => Some(vec![k as i32, *j]),
+            None=>{
+                kv.insert(v, k as i32);
+                None
+            }
+        }).unwrap()
+}
