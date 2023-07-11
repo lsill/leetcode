81183095bcd7c101198e30718cdf6b8a3e3d923d
zhizhen <lishuai@papegames.net>
2023-07-11 11:55:57 +0800

    1911. 最大子序列交替和

--- dp_demo/src/simple_dp.rs
+++ dp_demo/src/simple_dp.rs
@@ -47,3 +47,34 @@ pub fn maximum_sum_bc(arr: Vec<i32>) -> i32 {
     }
     res
 }
+
+/// 1911. 最大子序列交替和
+/// 一个下标从0开始的数组的交替和定义为 偶数下标处元素之和减去 奇数下标处元素之和。
+/// 比方说，数组[4,2,5,3]的交替和为(4 + 5) - (2 + 3) = 4。
+/// 给你一个数组nums，请你返回nums中任意子序列的最大交替和（子序列的下标 重新从 0 开始编号）。
+/// 一个数组的 子序列是从原数组中删除一些元素后（也可能一个也不删除）剩余元素不改变顺序组成的数组。
+/// 比方说，[2,7,4]是[4,2,3,7,2,1,4]的一个子序列（加粗元素），但是[2,4,2] 不是。
+/// [!链接](https://leetcode.cn/problems/maximum-alternating-subsequence-sum/solution/)
+
+
+// lc 评论解
+pub fn max_alternating_sum(nums: Vec<i32>) -> i64 {
+    let mut ori = 0;
+    let mut res = nums[0] as i64;
+
+    for i in 0..nums.len() {
+        res = std::cmp::max(res, ori + nums[i] as i64);
+        ori = std::cmp::max(ori, res - nums[i] as i64);
+    }
+    res
+}
+// lc 最优解
+pub fn max_alternating_sum_1(nums: Vec<i32>) -> i64 {
+    let (mut odd, mut even) = (0, nums[0] as i64);
+
+    for i in 1..nums.len() {
+        odd = odd.max(even - nums[i] as i64);
+        even = even.max(odd + nums[i] as i64);
+    }
+    even
+}
