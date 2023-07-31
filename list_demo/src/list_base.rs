23c1ac461b59f8cd106e4581cd1ca911a9ad270b
zhizhen <lishuai@papegames.net>
2023-07-31 11:40:12 +0800

    143. 重排链表

--- list_demo/src/list_base.rs
+++ list_demo/src/list_base.rs
@@ -118,3 +118,33 @@ pub fn reverse_list_no(head: &mut Option<Box<ListNode>>) {
     }
     *head = prev
 }
+
+/// 143. 重排链表
+/// 给定一个单链表 L 的头节点 head ，单链表 L 表示为：
+/// L0 → L1 → … → Ln - 1 → Ln
+/// 请将其重新排列后变为：
+/// L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+/// 不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+// 把链表存储到线性表中，然后用双指针依次从头尾取元素
+pub fn reorder_list(head: &mut Option<Box<ListNode>>) {
+    let mut nodes:Vec<Option<Box<ListNode>>> = Vec::new();
+    let mut curr = head.take();
+    while let Some(mut node) = curr{
+        curr = node.next.take();
+        nodes.push(Some(node));
+    }
+    let len = nodes.len();
+    let mid = len / 2;
+    let mut dummy = Box::new(ListNode::new(i32::default()));
+    let mut curr = &mut dummy;
+    for i in 0..mid {
+        curr.next = nodes[i].take();
+        curr = curr.next.as_mut().unwrap();
+        curr.next = nodes[len -i -1].take();
+        curr = curr.next.as_mut().unwrap();
+    }
+    if mid < len - mid {
+        curr.next = nodes[mid].take();
+    }
+    *head = dummy.next
+}
