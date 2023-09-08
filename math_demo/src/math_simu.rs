a083c01c0f142b0c30c72d7468f416be8dffc4f2
lsill <lishuai@papegames.net>
2023-09-08 10:29:12 +0800

    2651. 计算列车到站时间

--- math_demo/src/math_simu.rs
+++ math_demo/src/math_simu.rs
@@ -28,3 +28,12 @@ pub fn ways_to_buy_pens_pencils_1(total: i32, cost1: i32, cost2: i32) -> i64 {
     }
     res
 }
+
+/// 2651. 计算列车到站时间
+/// 给你一个正整数 arrivalTime 表示列车正点到站的时间（单位：小时），另给你一个正整数 delayedTime 表示列车延误的小时数。
+/// 返回列车实际到站的时间。
+/// 注意，该问题中的时间采用 24 小时制。
+
+pub fn find_delayed_arrival_time(arrival_time: i32, delayed_time: i32) -> i32 {
+    (arrival_time + delayed_time) % 24
+}
