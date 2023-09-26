322ddbc5bb5df3921b93c2539cab24fbd7f6b3e5
zhizhen <lishuai@papegames.net>
2023-09-26 11:59:25 +0800

    2582. 递枕头

--- math_demo/src/math_simu.rs
+++ math_demo/src/math_simu.rs
@@ -18,7 +18,7 @@ pub fn ways_to_buy_pens_pencils(total: i32, cost1: i32, cost2: i32) -> i64 {
 // lc 比较优秀的解
 pub fn ways_to_buy_pens_pencils_1(total: i32, cost1: i32, cost2: i32) -> i64 {
     if cost1 < cost2 {
-        return Self::ways_to_buy_pens_pencils(total, cost2, cost1)
+        return ways_to_buy_pens_pencils(total, cost2, cost1)
     }
     let mut res = 0_i64;
     let mut total = total;
@@ -36,4 +36,20 @@ pub fn ways_to_buy_pens_pencils_1(total: i32, cost1: i32, cost2: i32) -> i64 {
 
 pub fn find_delayed_arrival_time(arrival_time: i32, delayed_time: i32) -> i32 {
     (arrival_time + delayed_time) % 24
+}
+
+/// 2582. 递枕头
+/// n 个人站成一排，按从 1 到 n 编号。
+/// 最初，排在队首的第一个人拿着一个枕头。每秒钟，拿着枕头的人会将枕头传递给队伍中的下一个人。一旦枕头到达队首或队尾，传递方向就会改变，队伍会继续沿相反方向传递枕头。
+/// 例如，当枕头到达第 n 个人时，TA 会将枕头传递给第 n - 1 个人，然后传递给第 n - 2 个人，依此类推。
+/// 给你两个正整数 n 和 time ，返回 time 秒后拿着枕头的人的编号。
+pub fn pass_the_pillow(n: i32, time: i32) -> i32 {
+    let round = time / (n - 1);
+    let step = time % (n - 1);
+
+    if round & 1 == 1 {
+        n - step
+    } else {
+        1 + step
+    }
 }
