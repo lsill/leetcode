ebe473e20adbb31002402ab13cfead03edf2b76c
zhizhen <lishuai@papegames.net>
2023-10-09 11:11:31 +0800

    2578. 最小和分割

--- str_demo/src/char_demo.rs
+++ str_demo/src/char_demo.rs
@@ -302,3 +302,32 @@ pub fn reverse_string_0(s: &mut Vec<char>) {
     // }
     s.reverse();
 }
+
+/// 2578. 最小和分割
+/// 给你一个正整数 num ，请你将它分割成两个非负整数 num1 和 num2 ，满足：
+/// num1 和 num2 直接连起来，得到 num 各数位的一个排列。
+/// 换句话说，num1 和 num2 中所有数字出现的次数之和等于 num 中所有数字出现的次数。
+/// num1 和 num2 可以包含前导 0 。
+/// 请你返回 num1 和 num2 可以得到的和的 最小 值。
+/// 注意：
+/// num 保证没有前导 0 。
+/// num1 和 num2 中数位顺序可以与 num 中数位顺序不同。
+/// 示例 1：
+/// 输入：num = 4325
+/// 输出：59
+/// 解释：我们可以将 4325 分割成 num1 = 24 和 num2 = 35 ，和为 59 ，59 是最小和。
+/// 示例 2：
+/// 输入：num = 687
+/// 输出：75
+/// 解释：我们可以将 687 分割成 num1 = 68 和 num2 = 7 ，和为最优值 75 。
+
+// 力扣优秀解
+pub fn split_num(num: i32) -> i32 {
+    let mut s:Vec<u8> = num.to_string().bytes().collect();
+    s.sort_unstable();
+    let mut a = [0,0];
+    for (i, &c) in s.iter().enumerate() {
+        a[i % 2] = a[i % 2] * 10 + c as i32 - '0' as i32;
+    }
+    a[0] + a[1]
+}
