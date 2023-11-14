7383769de815b812b6842132d0484b744a1e9e18
zhizhen <lishuai@papegames.net>
2023-11-14 11:12:12 +0800

    1334. 阈值距离内邻居最少的城市

--- arr_demo/src/middle_dm.rs
+++ arr_demo/src/middle_dm.rs
@@ -1,6 +1,7 @@
 use std::arch::aarch64::{vaba_s8, veor_s8};
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::hash_map::VacantEntry;
 use std::iter::from_fn;
 use std::panic::resume_unwind;
 use std::process::abort;
@@ -200,4 +201,197 @@ pub fn filter_restaurants(restaurants:Vec<Vec<i32>>, vegan_friendly: i32, max_pr
         .collect::<Vec<_>>();
     restaurants.sort_unstable_by_key(|r| (-r[1], -r[0]));
     restaurants.iter().map(|r| r[0]).collect()
+}
+
+
+/// 1334. 阈值距离内邻居最少的城市
+/// 有 n 个城市，按从 0 到 n-1 编号。给你一个边数组 edges，其中 edges[i] = [fromi, toi, weighti]
+/// 代表 fromi 和 toi 两个城市之间的双向加权边，距离阈值是一个整数 distanceThreshold。
+/// 返回能通过某些路径到达其他城市数目最少、且路径距离 最大 为 distanceThreshold 的城市。如果有多个这样的城市，则返回编号最大的城市。
+/// 注意，连接城市 i 和 j 的路径的距离等于沿该路径的所有边的权重之和。
+/// 示例 1：
+/// 输入：n = 4, edges = [[0,1,3],[1,2,1],[1,3,4],[2,3,1]], distanceThreshold = 4
+/// 输出：3
+/// 解释：城市分布图如上。
+/// 每个城市阈值距离 distanceThreshold = 4 内的邻居城市分别是：
+/// 城市 0 -> [城市 1, 城市 2]
+/// 城市 1 -> [城市 0, 城市 2, 城市 3]
+/// 城市 2 -> [城市 0, 城市 1, 城市 3]
+/// 城市 3 -> [城市 1, 城市 2]
+/// 城市 0 和 3 在阈值距离 4 以内都有 2 个邻居城市，但是我们必须返回城市 3，因为它的编号最大。
+/// 示例 2：
+///
+///输入：n = 5, edges = [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]], distanceThreshold = 2
+/// 输出：0
+/// 解释：城市分布图如上。
+/// 每个城市阈值距离 distanceThreshold = 2 内的邻居城市分别是：
+/// 城市 0 -> [城市 1]
+/// 城市 1 -> [城市 0, 城市 4]
+/// 城市 2 -> [城市 3, 城市 4]
+/// 城市 3 -> [城市 2, 城市 4]
+/// 城市 4 -> [城市 1, 城市 2, 城市 3]
+/// 城市 0 在阈值距离 2 以内只有 1 个邻居城市。
+/// 提示：
+/// 2 <= n <= 100
+/// 1 <= edges.length <= n * (n - 1) / 2
+/// edges[i].length == 3
+/// 0 <= fromi < toi < n
+/// 1 <= weighti, distanceThreshold <= 10^4
+/// 所有 (fromi, toi) 都是不同的。
+
+// 力扣未优化(会超时)
+pub fn find_the_city_1(n: i32, edges: Vec<Vec<i32>>, distance_threshold: i32) -> i32 {
+    let n = n as usize;
+    let mut w = vec![vec![i32::MAX / 2; n]; n]; // 除 2 防止加法溢出
+    for e in &edges {
+        let x = e[0] as usize;
+        let y = e[1] as usize;
+        let wt = e[2];
+        w[x][y] = wt;
+        w[y][x] = wt;
+    }
+
+    fn dfs(k: i32, i: usize, j: usize, w: &Vec<Vec<i32>>) -> i32 {
+        if k < 0 { // 递归边界
+            return w[i][j];
+        }
+        return dfs(k - 1, i, j, w).min(dfs(k - 1, i, k as usize, w) + dfs(k - 1, k as usize, j, w));
+    }
+
+    let mut ans = 0;
+    let mut min_cnt = n;
+    for i in 0..n {
+        let mut cnt = 0;
+        for j in 0..n {
+            if j != i && dfs(n as i32 - 1, i, j, &w) <= distance_threshold {
+                cnt += 1;
+            }
+        }
+        if cnt <= min_cnt { // 相等时取最大的 i
+            min_cnt = cnt;
+            ans = i;
+        }
+    }
+    ans as i32
+}
+
+// 力扣第一波优化 递归+记录返回值= 记忆化搜索
+// 如果一个状态（递归入参）是第一次遇到，那么可以在返回前，把状态及其结果记到一个 memo数组中。
+// 如果一个状态不是第一次遇到（memo 中保存的结果不等于 memo的初始值），那么可以直接返回 memo中保存的结果。
+pub fn find_the_city_2(n: i32, edges: Vec<Vec<i32>>, distance_threshold: i32) -> i32 {
+    let n = n as usize;
+    let mut w = vec![vec![i32::MAX / 2; n]; n]; // 除 2 防止加法溢出
+    for e in &edges {
+        let x = e[0] as usize;
+        let y = e[1] as usize;
+        let wt = e[2];
+        w[x][y] = wt;
+        w[y][x] = wt;
+    }
+
+    let mut memo = vec![vec![vec![0; n]; n]; n];
+    fn dfs(k: i32, i: usize, j: usize, w: &Vec<Vec<i32>>, memo: &mut Vec<Vec<Vec<i32>>>) -> i32 {
+        if k < 0 { // 递归边界
+            return w[i][j];
+        }
+        if memo[k as usize][i][j] != 0 { // 之前计算过
+            return memo[k as usize][i][j];
+        }
+        let res = dfs(k - 1, i, j, w, memo).min(dfs(k - 1, i, k as usize, w, memo) + dfs(k - 1, k as usize, j, w, memo));
+        memo[k as usize][i][j] = res; // 记忆化
+        res
+    }
+
+    let mut ans = 0;
+    let mut min_cnt = n;
+    for i in 0..n {
+        let mut cnt = 0;
+        for j in 0..n {
+            if j != i && dfs(n as i32 - 1, i, j, &w, &mut memo) <= distance_threshold {
+                cnt += 1;
+            }
+        }
+        if cnt <= min_cnt { // 相等时取最大的 i
+            min_cnt = cnt;
+            ans = i;
+        }
+    }
+    ans as i32
+}
+
+// 力扣优化 1:1翻译成堆
+pub fn find_the_city_3(n: i32, edges: Vec<Vec<i32>>, distance_threshold: i32) -> i32 {
+    let n = n as usize;
+    let mut w = vec![vec![i32::MAX / 2; n]; n]; // 除 2 防止加法溢出
+    for e in &edges {
+        let x = e[0] as usize;
+        let y = e[1] as usize;
+        let wt = e[2];
+        w[x][y] = wt;
+        w[y][x] = wt;
+    }
+
+    let mut f = vec![vec![vec![0; n]; n]; n + 1];
+    f[0] = w;
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                f[k + 1][i][j] = f[k][i][j].min(f[k][i][k] + f[k][k][j]);
+            }
+        }
+    }
+
+    let mut ans = 0;
+    let mut min_cnt = n;
+    for i in 0..n {
+        let mut cnt = 0;
+        for j in 0..n {
+            if j != i && f[n][i][j] <= distance_threshold {
+                cnt += 1;
+            }
+        }
+        if cnt <= min_cnt { // 相等时取最大的 i
+            min_cnt = cnt;
+            ans = i;
+        }
+    }
+    ans as i32
+}
+
+// 力扣空间优化版
+pub fn find_the_city(n: i32, edges: Vec<Vec<i32>>, distance_threshold: i32) -> i32 {
+    let n = n as usize;
+    let mut w = vec![vec![i32::MAX / 2; n]; n]; // 除 2 防止加法溢出
+    for e in &edges {
+        let x = e[0] as usize;
+        let y = e[1] as usize;
+        let wt = e[2];
+        w[x][y] = wt;
+        w[y][x] = wt;
+    }
+
+    let mut f = w;
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                f[i][j] = f[i][j].min(f[i][k] + f[k][j]);
+            }
+        }
+    }
+
+    let mut ans = 0;
+    let mut min_cnt = n;
+    for i in 0..n {
+        let mut cnt = 0;
+        for j in 0..n {
+            if j != i && f[i][j] <= distance_threshold {
+                cnt += 1;
+            }
+        }
+        if cnt <= min_cnt { // 相等时取最大的 i
+            min_cnt = cnt;
+            ans = i;
+        }
+    }
+    ans as i32
 }
