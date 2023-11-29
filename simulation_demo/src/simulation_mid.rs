973231ee2d22797343a4b32b0d103e89871ead68
zhizhen <lishuai@papegames.net>
2023-11-29 14:46:01 +0800

    2336. 无限集中的最小数字

--- simulation_demo/src/simulation_mid.rs
+++ simulation_demo/src/simulation_mid.rs
@@ -1,4 +1,9 @@
-use std::collections::{BTreeMap, HashMap};
+use std::{
+    cmp::Reverse,
+    collections::{BTreeMap, HashMap}
+};
+use std::arch::aarch64::vaddlv_s16;
+use std::collections::{BinaryHeap, BTreeSet, HashSet};
 
 /// 2034. 股票价格波动
 /// 给你一支股票价格的数据流。数据流中每一条记录包含一个 时间戳 和该时间点股票对应的 价格 。
@@ -71,3 +76,94 @@ impl StockPrice {
  * let ret_3: i32 = obj.maximum();
  * let ret_4: i32 = obj.minimum();
  */
+
+
+/// 2336. 无限集中的最小数字
+/// 现有一个包含所有正整数的集合 [1, 2, 3, 4, 5, ...] 。
+/// 实现 SmallestInfiniteSet 类：
+/// SmallestInfiniteSet() 初始化 SmallestInfiniteSet 对象以包含 所有 正整数。
+/// int popSmallest() 移除 并返回该无限集中的最小整数。
+/// void addBack(int num) 如果正整数 num 不 存在于无限集中，则将一个 num 添加 到该无限集最后。
+///
+/// 示例：
+/// 输入
+/// ["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+/// [[], [2], [], [], [], [1], [], [], []]
+/// 输出
+/// [null, null, 1, 2, 3, null, 1, 4, 5]
+///
+/// 解释
+/// SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+/// smallestInfiniteSet.addBack(2);    // 2 已经在集合中，所以不做任何变更。
+/// smallestInfiniteSet.popSmallest(); // 返回 1 ，因为 1 是最小的整数，并将其从集合中移除。
+/// smallestInfiniteSet.popSmallest(); // 返回 2 ，并将其从集合中移除。
+/// smallestInfiniteSet.popSmallest(); // 返回 3 ，并将其从集合中移除。
+/// smallestInfiniteSet.addBack(1);    // 将 1 添加到该集合中。
+/// smallestInfiniteSet.popSmallest(); // 返回 1 ，因为 1 在上一步中被添加到集合中，
+///                                    // 且 1 是最小的整数，并将其从集合中移除。
+/// smallestInfiniteSet.popSmallest(); // 返回 4 ，并将其从集合中移除。
+/// smallestInfiniteSet.popSmallest(); // 返回 5 ，并将其从集合中移除。
+///
+/// 提示：
+/// 1 <= num <= 1000
+/// 最多调用 popSmallest 和 addBack 方法 共计 1000 次
+
+// 自己做的垃圾
+struct SmallestInfiniteSet1 {
+    kv:HashMap<i32, bool>
+}
+
+impl SmallestInfiniteSet1 {
+    fn new() -> Self {
+        SmallestInfiniteSet1{ kv: HashMap::new() }
+    }
+
+    fn pop_smallest(&mut self) ->i32 {
+        let mut ans:i32 = 0;
+        for i in 1..1001 {
+            if self.kv.contains_key(&i) {
+                continue
+            }
+            self.kv.insert(i, true);
+            ans = i;
+            break
+        }
+        ans
+    }
+
+    fn add_back(&mut self, num:i32) {
+        self.kv.remove(&num);
+    }
+}
+
+// 力扣的解
+struct SmallestInfiniteSet {
+    pop_nums:BTreeSet<i32>,
+    water_level:i32
+}
+
+impl SmallestInfiniteSet {
+    fn new() ->Self {
+        Self {
+            pop_nums:BTreeSet::new(),
+            water_level:1
+        }
+    }
+    fn pop_smallest(&mut self) -> i32 {
+        if self.pop_nums.is_empty() {
+            let ans = self.water_level;
+            self.water_level += 1;
+            return ans;
+        }
+        let mut ans = *self.pop_nums.iter().next().unwrap();
+        self.pop_nums.remove(&ans);
+        ans
+    }
+
+    fn add_back(&mut self, num:i32) {
+        if num >= self.water_level {
+            return;
+        }
+        self.pop_nums.insert(num);
+    }
+}
