ff1c4d77ad644e3210bee1787ec7d3606917156e
zhizhen <lishuai@papegames.net>
2023-12-04 11:27:59 +0800

    1038. 从二叉搜索树到更大和树

--- tree_demo/src/tree_pra.rs
+++ tree_demo/src/tree_pra.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
-use std::cell::{Ref, RefCell};
+use std::cell::{Ref, RefCell, RefMut};
+use std::ptr::{addr_of_mut, NonNull};
 use crate::tree_base::TreeNode;
 
 /// 617. 合并二叉树
@@ -43,4 +44,36 @@ pub fn merge_trees_1(root1: Option<Rc<RefCell<TreeNode>>>, root2: Option<Rc<RefC
         (None,some)=>some,
         (some, None)=>some,
     }
+}
+
+/// 1038. 从二叉搜索树到更大和树
+/// 给定一个二叉搜索树 root (BST)，请将它的每个节点的值替换成树中大于或者等于该节点值的所有节点值之和。
+/// 提醒一下， 二叉搜索树 满足下列约束条件：
+/// 节点的左子树仅包含键 小于 节点键的节点。
+/// 节点的右子树仅包含键 大于 节点键的节点。
+/// 左右子树也必须是二叉搜索树。
+/// 示例 1：
+/// 输入：[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+/// 输出：[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+/// 示例 2：
+/// 输入：root = [0,null,1]
+/// 输出：[1,null,1]
+/// 提示：
+/// 树中的节点数在 [1, 100] 范围内。
+/// 0 <= Node.val <= 100
+/// 树中的所有值均 不重复 。
+
+pub fn bst_to_gst(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+    fn dfs(node:Option<&Rc<RefCell<TreeNode>>>, s: &mut i32) {
+        if let Some(x) = node {
+            let mut x = x.borrow_mut();
+            dfs(x.right.as_ref(), s) ;
+            *s += x.val;
+            x.val = *s;
+            dfs(x.left.as_ref(), s);
+        }
+    }
+    let mut s = 0;
+    dfs(root.as_ref(), &mut s);
+    root
 }
