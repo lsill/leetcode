f5beaf843e10398f333e65cc1bfb53692f327317
lsill <924867750@qq.com>
2023-12-25 11:20:03 +0800

    1276. 不浪费原料的汉堡制作方案

--- math_demo/src/math_simu.rs
+++ math_demo/src/math_simu.rs
@@ -148,3 +148,64 @@ pub fn sum_of_multiples_1(n: i32) -> i32 {
     let s = |m:i32| n/m * (n/m + 1) / 2 * m;
     s(3) + s(5) + s(7) - s(15) - s(21) - s(35) + s(105)
 }
+
+/// 1276. 不浪费原料的汉堡制作方案
+/// 圣诞活动预热开始啦，汉堡店推出了全新的汉堡套餐。为了避免浪费原料，请你帮他们制定合适的制作计划。
+/// 给你两个整数 tomatoSlices 和 cheeseSlices，分别表示番茄片和奶酪片的数目。不同汉堡的原料搭配如下：
+/// 巨无霸汉堡：4 片番茄和 1 片奶酪
+/// 小皇堡：2 片番茄和 1 片奶酪
+/// 请你以 [total_jumbo, total_small]（[巨无霸汉堡总数，小皇堡总数]）的格式返回恰当的制作方案，使得剩下的番茄片 tomatoSlices 和奶酪片 cheeseSlices 的数量都是 0。
+/// 如果无法使剩下的番茄片 tomatoSlices 和奶酪片 cheeseSlices 的数量为 0，就请返回 []。
+/// 示例 1：
+/// 输入：tomatoSlices = 16, cheeseSlices = 7
+/// 输出：[1,6]
+/// 解释：制作 1 个巨无霸汉堡和 6 个小皇堡需要 4*1 + 2*6 = 16 片番茄和 1 + 6 = 7 片奶酪。不会剩下原料。
+/// 示例 2：
+/// 输入：tomatoSlices = 17, cheeseSlices = 4
+/// 输出：[]
+/// 解释：只制作小皇堡和巨无霸汉堡无法用光全部原料。
+/// 示例 3：
+/// 输入：tomatoSlices = 4, cheeseSlices = 17
+/// 输出：[]
+/// 解释：制作 1 个巨无霸汉堡会剩下 16 片奶酪，制作 2 个小皇堡会剩下 15 片奶酪。
+/// 示例 4：
+/// 输入：tomatoSlices = 0, cheeseSlices = 0
+/// 输出：[0,0]
+/// 示例 5：
+/// 输入：tomatoSlices = 2, cheeseSlices = 1
+/// 输出：[0,1]
+/// 提示：
+/// 0 <= tomatoSlices <= 10^7
+/// 0 <= cheeseSlices <= 10^7
+
+// 自己做的
+pub fn num_of_burgers_1(tomato_slices: i32, cheese_slices: i32) -> Vec<i32> {
+    let mut ans:Vec<i32> = Vec::new();
+    if tomato_slices & 1 == 1 {
+        return ans;
+    }
+    if cheese_slices == 0 {
+        return if tomato_slices == 0 {
+            vec![0, 0]
+        } else {
+            ans
+        }
+    }
+    let x = tomato_slices / 2 - cheese_slices;
+    if x < 0 || cheese_slices - x < 0{
+        return ans;
+    }
+    return vec![x, cheese_slices - x]
+}
+
+// 写的比较优雅的
+pub fn num_of_burgers(tomato_slices: i32, cheese_slices: i32) -> Vec<i32> {
+    if tomato_slices < cheese_slices * 2
+        || tomato_slices > cheese_slices * 4
+        || tomato_slices & 1 == 1 {
+        return vec![]
+    } else {
+        let big = (tomato_slices - cheese_slices * 2) /2;
+        return vec![big, cheese_slices - big]
+    }
+}
