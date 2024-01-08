875f2a9c82b87fa117d7ba16464266461dbab993
lsill <924867750@qq.com>
2024-01-08 11:30:24 +0800

    447. 回旋镖的数量

--- matrix_demo/readme.md
+++ matrix_demo/readme.md
@@ -1,2 +1,3 @@
 - [2397. 被列覆盖的最多行数](https://github.com/lsill/leetcode/blob/main/matrix_demo/src/matrix_middle.rs) 很有深意的题，
 用到了二进制或运算，蛮好的 
+- [447. 回旋镖的数量](https://github.com/lsill/leetcode/blob/main/matrix_demo/src/matrix_middle.rs) 题难读懂，是迭代遍历题，可以看下
--- matrix_demo/src/matrix_middle.rs
+++ matrix_demo/src/matrix_middle.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::mem::swap;
+
 /// 2397. 被列覆盖的最多行数
 /// 给你一个下标从 0 开始、大小为 m x n 的二进制矩阵 matrix ；另给你一个整数 numSelect，表示你必须从 matrix 中选择的 不同 列的数量。
 /// 如果一行中所有的 1 都被你选中的列所覆盖，则认为这一行被 覆盖 了。
@@ -65,6 +68,43 @@ pub fn maximum_rows_1(matrix: Vec<Vec<i32>>, num_select: i32) -> i32 {
             }
         }
         ans = ans.max(c);
+    }
+    ans
+}
+
+/// 447. 回旋镖的数量
+/// 给定平面上 n 对 互不相同 的点 points ，其中 points[i] = [xi, yi] 。回旋镖 是由点 (i, j, k) 表示的元组
+/// ，其中 i 和 j 之间的距离和 i 和 k 之间的欧式距离相等（需要考虑元组的顺序）。
+/// 返回平面上所有回旋镖的数量。
+/// 示例 1：
+/// 输入：points = [[0,0],[1,0],[2,0]]
+/// 输出：2
+/// 解释：两个回旋镖为 [[1,0],[0,0],[2,0]] 和 [[1,0],[2,0],[0,0]]
+/// 示例 2：
+/// 输入：points = [[1,1],[2,2],[3,3]]
+/// 输出：2
+/// 示例 3：
+/// 输入：points = [[1,1]]
+/// 输出：0
+/// 提示：
+/// n == points.length
+/// 1 <= n <= 500
+/// points[i].length == 2
+/// -104 <= xi, yi <= 104
+/// 所有点都 互不相同
+
+// 力扣比较好的题解
+pub fn number_of_boomerangs(points: Vec<Vec<i32>>) -> i32 {
+    let mut ans = 0;
+    let mut cnt = HashMap::new();
+    for p1 in &points {
+        cnt.clear();
+        for p2 in &points {
+            let d2 = (p1[0] - p2[0]) *(p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]);
+            let mut v = cnt.entry(d2).or_insert(0);
+            ans += *v * 2;
+            *v += 1;
+        }
     }
     ans
 }
